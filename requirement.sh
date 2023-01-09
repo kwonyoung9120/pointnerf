d4d8ba806d3ba5c9229c5632e1a86d3dc5f0f4bb
Kwonyoung Ryu <kwonyoung9120@gmail.com>
2023-01-09 11:16:53 +0900

    update postech dataset

--- requirement.sh
+++ requirement.sh
@@ -1,2 +1,10 @@
-pip install torch==1.8.1+cu102 h5py
-pip install imageio scikit-image
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC
+apt update
+apt upgrade
+apt install python3-pip 
+pip3 install --upgrade pip 
+pip3 install torch torchvision h5py imageio scikit-image opencv-python==4.6.0.66 torch_scatter kornia importlib_metadata warmup_scheduler 
+apt-get -y install libgl1-mesa-glx 
+python3 -m pip install --upgrade setuptools 
+pip3 install inplace_abn --no-binary :all: 
+pip3 install tqdm lpips open3d plyfile pycuda
